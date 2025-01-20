080b8020a5ef134d7411dc9872bc08e1a1fcb6a7
Kotaro Kamashima <kotaro0726@gmail.com>
2025-01-20 20:03:30 +0900

    feat: Create MainPageWithDisabledBottomBar

--- lib/ui/main_page_with_disabled_bottom_bar.dart
+++ lib/ui/main_page_with_disabled_bottom_bar.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/domain/model/permission.dart';
 import 'package:yumemi_grow_lightning_talk_20250124/domain/model/user.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/ui/chat_page.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/ui/dashboard_page.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/ui/settings_page.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/ui/students_page.dart';
 
-class MainPage extends HookWidget {
-  const MainPage({
+class MainPageWithDisabledBottomBar extends HookWidget {
+  const MainPageWithDisabledBottomBar({
     super.key,
     required User user,
   }) : _user = user;
@@ -12,14 +17,17 @@ class MainPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     final currentPageIndex = useState(0);
+    final isDashboardEnabled = _user.permissions.contains(Permission.dashboard);
+    final isStudentsEnabled = _user.permissions.contains(Permission.students);
+    final isChatEnabled = _user.permissions.contains(Permission.chat);
+    final isSettingsEnabled = _user.permissions.contains(Permission.setting);
 
     final pages = [
-      Center(child: Text("Dashboard Page", style: textTheme.headlineMedium)),
-      Center(child: Text('Students Page', style: textTheme.headlineMedium)),
-      Center(child: Text('Chat Page', style: textTheme.headlineMedium)),
-      Center(child: Text('Settings Page', style: textTheme.headlineMedium)),
+      DashboardPage(),
+      StudentsPage(),
+      ChatPage(),
+      SettingsPage(),
     ];
 
     return Scaffold(
@@ -32,26 +40,30 @@ class MainPage extends HookWidget {
           currentPageIndex.value = index;
         },
         selectedIndex: currentPageIndex.value,
-        destinations: const [
+        destinations: [
           NavigationDestination(
             icon: Icon(Icons.list_outlined),
             selectedIcon: Icon(Icons.list),
             label: 'Dashboard',
+            enabled: isDashboardEnabled,
           ),
           NavigationDestination(
             icon: Icon(Icons.groups_outlined),
             selectedIcon: Icon(Icons.groups),
             label: 'Students',
+            enabled: isStudentsEnabled,
           ),
           NavigationDestination(
             icon: Icon(Icons.chat_outlined),
             selectedIcon: Icon(Icons.chat),
             label: 'Chat',
+            enabled: isChatEnabled,
           ),
           NavigationDestination(
             icon: Icon(Icons.settings_outlined),
             selectedIcon: Icon(Icons.settings),
             label: 'Settings',
+            enabled: isSettingsEnabled,
           ),
         ],
       ),
