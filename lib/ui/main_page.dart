7ad7fe307513156c5b46ae61cc9a200e92fbdd58
Kotaro Kamashima <kotaro0726@gmail.com>
2025-01-20 19:25:19 +0900

    feat: Obtain logged in user data

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/domain/model/user.dart';
 
 class MainPage extends HookWidget {
-  const MainPage({super.key});
+  const MainPage({
+    super.key,
+    required User user,
+  }) : _user = user;
+
+  final User _user;
 
   @override
   Widget build(BuildContext context) {
