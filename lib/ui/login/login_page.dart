b6dc012eedae401e7b25b34d1b05ce1860d90c05
Kotaro Kamashima <kotaro0726@gmail.com>
2025-01-22 19:57:48 +0900

    modify: Implement goToMainPage method

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:yumemi_grow_lightning_talk_20250124/domain/model/mock_users.dart';
 import 'package:yumemi_grow_lightning_talk_20250124/domain/model/user.dart';
-import 'package:yumemi_grow_lightning_talk_20250124/ui/main_page.dart';
+import 'package:yumemi_grow_lightning_talk_20250124/ui/main/main_page_with_hidden_bottom_bar.dart';
 
 class LoginPage extends HookWidget {
   const LoginPage({super.key});
@@ -12,6 +12,26 @@ class LoginPage extends HookWidget {
     final users = mockUsers;
     // State for selected user
     final selectedUser = useState(users[0]);
+
+    void goToMainPage(BuildContext context) {
+      final user = selectedUser.value;
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) {
+          /// Testing hiding bottom tabs when permissions are lacking.
+          return MainPageWithHiddenBottomBar(
+            user: user,
+          );
+
+          /// Testing disabling bottom tabs when permissions are lacking.
+          // return MainPageWithDisabledBottomBar(
+          //   user: user,
+          // );
+
+          /// Testing showing No Permission Page when permissions are lacking.
+          // return MainPageWithNoPermission(user: user);
+        }),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -38,16 +58,7 @@ class LoginPage extends HookWidget {
             ),
             const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: () {
-                // Go to MainPage.
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => MainPage(
-                      user: selectedUser.value,
-                    ),
-                  ),
-                );
-              },
+              onPressed: () => goToMainPage(context),
               child: const Text("Login"),
             ),
           ],
